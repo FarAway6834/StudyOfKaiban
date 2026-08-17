5c8180caaf96ec7bca71a08ea9f7e7e475aa8214
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-08-17 14:08:31 +0900

    StudyOfKaiban.py 업데이트

--- StudyOfKaiban.py
+++ StudyOfKaiban.py
@@ -1,1 +1,22 @@
-from collections import deque;mver = lambda dx, dy : lambda x, y, i : (x + dx[i], y + dy[i]);in_range = lambda x, n : (0 <= x < n);direction = lambda n, p = 0 : ([[-1, 1, 0, 0, -1, -1, 1, 1], [-1, -1, 0, 1, 1, 1, 0, -1], [0, 1, 0, -1]][p][:n + 1], [[0, 0, -1, 1, -1, 1, -1, 1], [0, 1, 1, 1, 0, -1, -1, -1], [1, 0, -1, 0]][p][:n + 1]);basic_tool = lambda N, p = 0 : (lambda dx, dy : (dx, dy, mver(dx, dy), (lambda x, y, n, m : in_range(x, n) and in_range(y, m)), range(N)))(*direction(N, p = p));from subpr.lib import martialaw as _clsr;from subpr.lib import comp as _comp;hanten = _comp(list, _clsr(map)(list), zip);turn90 = lambda x : hanten(*x[::-1])
+from collections import deque
+
+def asserter(p, /, msg = None, *, work = None):
+    if msg == None: assert p
+    else: assert p, msg
+    if work != None: return work()
+
+mver = lambda dx, dy : lambda x, y, i : (x + dx[i], y + dy[i])
+in_range = lambda x, n : (0 <= x < n)
+direction = lambda n, p = 0 : ([[-1, 1, 0, 0, -1, -1, 1, 1], [-1, -1, 0, 1, 1, 1, 0, -1], [0, 1, 0, -1]][p][:n + 1], [[0, 0, -1, 1, -1, 1, -1, 1], [0, 1, 1, 1, 0, -1, -1, -1], [1, 0, -1, 0]][p][:n + 1])
+check = lambda *argv : (lambda Q, R : asserter(R == 0, "check get even number of arguments", work = (lambda : in_range(argv[0], argv[Q]) and checker(*argv[1:Q], *argv[Q + 1:]))))(*divmod(len(argv), 2))
+under = lambda x, *argv : is_range(x, argv[-1]) and under(*argv) if len(argv) else True
+
+basic_tool = lambda dx, dy, N = None : (dx, dy, mver(dx, dy), range(len(dx) if N == None else N))
+
+direc_tool = lambda N, p = 0 : (lambda dx, dy : basic_tool(dx, dy, N = N))(*direction(N, p = p))
+
+from subpr.lib import martialaw as _clsr
+from subpr.lib import comp as _comp
+
+hanten = _comp(list, _clsr(map)(list), zip)
+turn90 = lambda x : hanten(*x[::-1])
