915babe55c16aa9ab9a54ec62b2298d02fdc0c2b
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-08-17 10:17:39 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='StudyOfKaiban',
+    version='0.0.1',
+    description='read descript thag writed by korean what write in github.',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/BlindRunaway',
+    install_requires=['blru'],
+    py_modules=["StudyOfKaiban"],
+    python_requires='>=3.6',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
